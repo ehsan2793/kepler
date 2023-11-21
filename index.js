50d7ddc152adca03cd1ad570cfc0b23737c60881
ehsan2793 <ehsan2793@gmail.com>
2023-11-21 06:09:55 -0800

    only 8 planet

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("./kepler.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    let res = habitablePlanet.map((planet) => {
+      return planet["kepler_name"];
+    });
+    console.log(res);
+    // console.log(habitablePlanet);
+  });
